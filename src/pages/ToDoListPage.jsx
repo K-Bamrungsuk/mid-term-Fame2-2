8fb59f0c61b04a4b34f4bb99521d43ae735673b5
K-Bamrungsuk <famefame1965@gmail.com>
2026-07-14 21:08:35 +0700

    second commit

--- src/pages/ToDoListPage.jsx
+++ src/pages/ToDoListPage.jsx
@@ -1,14 +1,35 @@
-import { Navigate, useLoaderData } from "react-router";
+import { data, Navigate, useLoaderData } from "react-router";
 import { useUserStore } from "../stores/userStore";
 import api from "../api/mainApi";
 import { getAllList } from "../api/toDoListApi";
+import { useState } from "react";
+import axios from "axios";
 
 function ToDoListPage() {
   const token = useUserStore((state) => state.token);
   const logout = useUserStore((state) => state.logout);
   const output = useLoaderData();
+  console.log('output', output)
 
-  if (!token) return <Navigate to="/" replace />;
+  const [input, setInput] = useState()
+  
+  const hdlAdd = async () => {
+    try {
+        const add = await api.post("/todos/2", { content: input })
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+  const hdlDelete = async (el) => {
+    try {
+      const del = await api.delete(`/todos/2/${el.id}`)
+      console.log('del.data', del.data)
+    } catch (error){
+      console.log(error)
+    }
+  }
+
   return (
     <div className="m-auto w-full h-screen bg-sky-50 flex justify-center items-center">
       <div className="w-140 h-120 border rounded-2xl bg-white">
@@ -19,12 +40,40 @@ function ToDoListPage() {
         </div>
 
         <div className="flex justify-between p-4 border-b border-gray-300">
-          <h1 className="text-md py-2 px-6 text-gray-500 ml-5">new task</h1>
-          <button className="text-md py-2 px-6 rounded-3xl text-white bg-sky-600 mr-5">Add</button>
+          <input
+          onChange={(e)=> {setInput(e.target.value)}} 
+          className="text-md py-2 px-6 text-gray-700 ml-5 text-lg" 
+          type="text"
+          placeholder="new task"
+          />
+          <button 
+          className="text-md py-2 px-6 rounded-3xl text-white bg-sky-600 mr-5 cursor-pointer"
+          onClick={hdlAdd}
+          >Add</button>
         </div>
 
         <div className="flex flex-col gap-5 mt-6">
-          {output.map((el, index) => ( <p key={index}>{el?.content}</p>))}
+
+          {output.map((el, index) => (
+
+            <div  key={index} className="flex gap-4 mx-4 justify-between">
+              <div className="flex">
+                <input className="w-10" type="checkbox" />
+                <p>{el?.content}</p>
+              </div>
+              
+              <div className="flex gap-8">
+                <button
+                className="text-gray-500"
+                >edit</button>
+                <button
+                className="text-gray-500 cursor-pointer hover:text-black"
+                onClick={()=>hdlDelete(el)}
+                >x</button>
+              </div>
+              
+            </div> ))}
+          
         </div>
       </div>
     </div>
